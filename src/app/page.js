f52504f521214b33f2d362ceef84ebbdf4a3545b
Alexander Välitalo <143487921+AlexanderValitalo@users.noreply.github.com>
2024-04-22 16:26:00 +0200

    Add requirements.md

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Get started&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
@@ -85,9 +85,7 @@ export default function Home() {
           <h2>
             Deploy <span>-&gt;</span>
           </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
         </a>
       </div>
     </main>
